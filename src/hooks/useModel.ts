8e2c62eb1d53da6fb88da3071ede1696916a12c6
原罪 <531362022@qq.com>
2021-11-19 18:28:33 +0800

    feat(useModel): dynamically choose compare algorithm

--- src/hooks/useModel.ts
+++ src/hooks/useModel.ts
@@ -1,6 +1,16 @@
 import isEqual from 'lodash.isequal';
+import { shallowEqual } from 'react-redux';
 import { Model } from '../model/defineModel';
 import { useCustomSelector } from './useCustomSelector';
+
+/**
+ * hooks新旧数据的对比方式：
+ *
+ * - `deepEqual`     深度对比，对比所有层级的内容。传递selector时默认使用。
+ * - `shallowEqual`  浅对比，只比较对象第一层。传递多个模型但没有selector时默认使用。
+ * - `strictEqual`   全等（===）对比。只传一个模型但没有selector时默认使用。
+ */
+export type CompareType = 'strictEqual' | 'shallowEqual' | 'deepEqual';
 
 /**
  * * 获取模型的状态数据。
@@ -10,10 +20,12 @@ import { useCustomSelector } from './useCustomSelector';
  */
 export function useModel<State extends object>(
   model: Model<string, State>,
+  compare?: CompareType,
 ): State;
 export function useModel<State extends object, T>(
   model: Model<any, State>,
   selector: (state: State) => T,
+  compare?: CompareType,
 ): T;
 
 export function useModel<
@@ -24,6 +36,7 @@ export function useModel<
 >(
   model1: Model<Name1, State1>,
   model2: Model<Name2, State2>,
+  compare?: CompareType,
 ): {
   [K in Name1]: State1;
 } & {
@@ -39,6 +52,7 @@ export function useModel<
   model1: Model<Name1, State1>,
   model2: Model<Name2, State2>,
   selector: (state1: State1, state2: State2) => T,
+  compare?: CompareType,
 ): T;
 
 export function useModel<
@@ -52,6 +66,7 @@ export function useModel<
   model1: Model<Name1, State1>,
   model2: Model<Name2, State2>,
   model3: Model<Name3, State3>,
+  compare?: CompareType,
 ): {
   [K in Name1]: State1;
 } & {
@@ -72,6 +87,7 @@ export function useModel<
   model2: Model<Name2, State2>,
   model3: Model<Name3, State3>,
   selector: (state1: State1, state2: State2, state3: State3) => T,
+  compare?: CompareType,
 ): T;
 
 export function useModel<
@@ -88,6 +104,7 @@ export function useModel<
   model2: Model<Name2, State2>,
   model3: Model<Name3, State3>,
   model4: Model<Name4, State4>,
+  compare?: CompareType,
 ): {
   [K in Name1]: State1;
 } & {
@@ -118,6 +135,7 @@ export function useModel<
     state3: State3,
     state4: State4,
   ) => T,
+  compare?: CompareType,
 ): T;
 
 export function useModel<
@@ -137,6 +155,7 @@ export function useModel<
   model3: Model<Name3, State3>,
   model4: Model<Name4, State4>,
   model5: Model<Name5, State5>,
+  compare?: CompareType,
 ): {
   [K in Name1]: State1;
 } & {
@@ -173,29 +192,64 @@ export function useModel<
     state4: State4,
     state5: State5,
   ) => T,
+  compare?: CompareType,
 ): T;
 
-export function useModel(...args: any[]): any {
-  const models = args as Model<any, any>[];
-  const selector: Function | undefined =
-    typeof args[args.length - 1] === 'function' ? args.pop() : void 0;
+export function useModel(): any {
+  const args = slice.call(arguments);
+  let compareType: CompareType | false =
+    getLastElementType(args) === 'string' && args.pop();
+  const selector: Function | false =
+    getLastElementType(args) === 'function' && args.pop();
+  const models: Model<any, any>[] = args;
 
-  return useCustomSelector((state) => {
+  if (!compareType) {
     if (selector) {
-      return selector.apply(
-        null,
-        models.map((model) => state[model.name]),
-      );
+      // 返回子集或者计算过的内容，数据量相对较少。
+      compareType = 'deepEqual';
+    } else if (models.length > 1) {
+      // { key => model } 集合。
+      compareType = 'shallowEqual';
+    } else {
+      // 一个model属于一个reducer，reducer已经使用了深对比来判断是否变化，
+      // 所以使用时只需要全等判断。
+      compareType = 'strictEqual';
+    }
+  }
+
+  return useCustomSelector((state: Record<string, object>) => {
+    if (selector) {
+      const result: object[] = [];
+      for (let i = 0; i < models.length; ++i) {
+        result.push(state[models[i]!.name]!);
+      }
+      return selector.apply(null, result);
     }
 
     if (models.length === 1) {
       return state[models[0]!.name];
     }
 
-    const result: Record<string, any> = {};
-    models.forEach((item) => {
-      result[item.name] = state[item.name];
-    });
+    const result: typeof state = {};
+    for (let i = 0; i < models.length; ++i) {
+      const reducerName = models[i]!.name;
+      result[reducerName] = state[reducerName]!;
+    }
     return result;
-  }, isEqual);
+  }, compareFn[compareType]);
 }
+
+const compareFn: Record<
+  CompareType,
+  undefined | ((previous: any, next: any) => boolean)
+> = {
+  deepEqual: isEqual,
+  shallowEqual: shallowEqual,
+  strictEqual: void 0,
+};
+
+const slice = Array.prototype.slice;
+
+const getLastElementType = (args: any[]) => {
+  return args.length > 1 && typeof args[args.length - 1];
+};
