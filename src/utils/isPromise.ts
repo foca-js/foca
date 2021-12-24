af25375f3f41cddcb7fe79b66e3cde0b87b9d1b6
原罪 <531362022@qq.com>
2021-12-24 23:29:50 +0800

    feat: stop checking catch property from method isPromise

--- src/utils/isPromise.ts
+++ src/utils/isPromise.ts
@@ -1,13 +1,8 @@
-/**
- * Copy from p-is-promise which only support ESM
- * @see https://www.npmjs.com/package/p-is-promise
- */
 export const isPromise = <T>(value: any): value is Promise<T> => {
   return (
     value instanceof Promise ||
     (value !== null &&
       (typeof value === 'object' || typeof value === 'function') &&
-      typeof value.then === 'function' &&
-      typeof value.catch === 'function')
+      typeof value.then === 'function')
   );
 };
