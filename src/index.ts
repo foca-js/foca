93547caa61f56f7d5bca24fc44bed47c56807568
geekact <fanwenhua1990@gmail.com>
2022-05-14 22:47:19 +0800

    feat: never export combine method due to nobody will use it

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-// Basic
+// 模型中使用
 export { defineModel } from './model/defineModel';
+export { cloneModel } from './model/cloneModel';
+
+// 组件中使用
 export { useModel } from './api/useModel';
 export { useLoading } from './api/useLoading';
 export { getLoading } from './api/getLoading';
 export { connect } from './api/connect';
 export { useComputed } from './api/useComputed';
 
-// Init
+// 入口使用
+export { compose } from 'redux';
 export { modelStore as store } from './store/modelStore';
 export { FocaProvider } from './redux/FocaProvider';
 export { engines } from './engines';
-export { compose } from 'redux';
 
-// Advanced
-export { combine } from './store/proxyStore';
-export { cloneModel } from './model/cloneModel';
-
-// Types
+// 可能用到的TS类型
 export type {
   Action,
   AnyAction,
--- src/store/proxyStore.ts
+++ src/store/proxyStore.ts
@@ -9,11 +9,7 @@ const dispatch = () => {
 };
 
 /**
- * 绑定项目里共存的状态库的store。同时满足下列条件才有意义：
- *
- * 1. 在项目中用到 connect() 高阶组件
- * <br>
- * 2. 共存库也是基于redux和react-redux
+ * 为了触发connect()，需要将实体store都注册到代理的store。
  */
 export const combine = (otherStore: Store) => {
   otherStore.subscribe(dispatch);
