cdc5e3ce50dc942aa1dbfbce35ebfc35f50ff558
geekact <fanwenhua1990@gmail.com>
2022-12-24 10:56:50 +0800

    feat: drop IE

--- src/utils/immer.ts
+++ src/utils/immer.ts
@@ -1,9 +1,9 @@
 import { Immer, enableES5 } from 'immer';
 
 /**
- * 支持ES5
+ * 支持ES5，毕竟Proxy无法polyfill。
  * @link https://immerjs.github.io/immer/docs/installation#pick-your-immer-version
- * @since immer 6.0
+ * @since immer@6.0
  */
 enableES5();
 
