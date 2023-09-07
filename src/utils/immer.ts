b928b47f423cc977b0ea995a09e52185086ddf3b
geekact <fanwenhua1990@gmail.com>
2023-09-07 21:27:18 +0800

    revert: totally support es5 (#41)

--- src/utils/immer.ts
+++ src/utils/immer.ts
@@ -1,4 +1,11 @@
-import { Immer } from 'immer';
+import { Immer, enableES5 } from 'immer';
+
+/**
+ * 支持ES5，毕竟Proxy无法polyfill。有些用户手机可以10年不换！！
+ * @link https://immerjs.github.io/immer/docs/installation#pick-your-immer-version
+ * @since immer@6.0
+ */
+enableES5();
 
 export const immer = new Immer({
   autoFreeze: false,
